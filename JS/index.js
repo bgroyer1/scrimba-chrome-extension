518caba52c49d5610ed156746c936dc09c64d458
bgroyer1 <royerbraedan@gmail.com>
2024-01-09 13:18:02 -0800

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -9,14 +9,14 @@ const deleteBtn = document.querySelector("#delete-btn")
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 })
 
 deleteBtn.addEventListener("click", () => {
@@ -24,17 +24,17 @@ deleteBtn.addEventListener("click", () => {
     if (confirm === true) {
         myLeads = []
         localStorage.clear()
-        ulEl.textContent = null
+        render(myLeads)
     } 
 })
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
